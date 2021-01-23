ca90e1f4bc1348df1084553f2619d6c62ad9e746
Hoshi Ran <ureshinostyle350@gmail.com>
2021-01-24 03:34:08 +0900

    [fix] referctoring main.py

--- scr/main.py
+++ scr/main.py
@@ -3,24 +3,24 @@ import nlp
 import wc
 
 
-ARTISTS_NAME = '椎名林檎'
-SONG_NAME = '丸の内サディスティック'  # 全曲解析したいときは*を入力
-is_remove_title = 'yes'  # 曲名は除外して出力するか yes/no
+def main():
+    artist = '椎名林檎'
+    song = '丸の内サディスティック'  # 全曲解析 -> '*'を入力
+    is_remove_title = False  # 曲名を除外して出力 -> true
 
-url = 'https://www.uta-net.com/search/?Aselect=1&Keyword=' + \
-    ARTISTS_NAME+'&Bselect=1&sort=4&pnum=1'
-base_url = 'https://www.uta-net.com'   # 曲ページの先頭アドレス
+    # 歌詞の取得
+    lyrics = scraping.get_lyrics(artist_name=artist, song_name=song)
+    print(lyrics)
 
-rm_wd_list = ['ない', 'ある', 'なっ', 'あっ']  # 意味をなさないような単語を除外
+    # 形態素分析して単語選択
+    word_list = nlp.get_words(lyrics=lyrics,
+                              title=song,
+                              is_rm_title=is_remove_title)
+    print(word_list)
+
+    # wordcloud画像の生成と保存
+    wc.generate_wc(word_list, artist=artist, title=song)
 
 
 if __name__ == '__main__':
-    # 歌詞の取得
-    lyrics = scraping.get_lyrics(ARTISTS_NAME, SONG_NAME, url, base_url)
-
-    # 形態素分析して単語選択
-    word_list = nlp.get_word_list(
-        lyrics, SONG_NAME, rm_wd_list, is_remove_title)
-
-    # wordcloud画像の出力
-    wc.generate_wc(word_list, ARTISTS_NAME, SONG_NAME)
+    main()
