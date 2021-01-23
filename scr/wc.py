7b710308bf39e3288c771c6d19474bb6604e4134
Hoshi Ran <ureshinostyle350@gmail.com>
2021-01-24 01:38:30 +0900

    [fix] refactoring wc.py

--- scr/wc.py
+++ scr/wc.py
@@ -1,26 +1,25 @@
 from wordcloud import WordCloud
 
 
-def generate_wc(word_list,  ARTISTS_NAME, SONG_NAME):
+def generate_wc(words: list,  artist: str, title: str):
     ## -----*----- wordcloud画像の生成、保存 -----*----- ##
-    '''
-    word_list : 出力単語
-    SONG_NAME : 曲名
-    ARTISTS_NAME : 歌手名
-    '''
-    text = ' '.join(word_list)     # 形態素解析された単語のリストをWordCloud用に処理
+
+    text = ' '.join(words)     # 単語リストをWordCloud用に整形
 
     # フォントを指定
-    fpath = "/usr/share/fonts/truetype/takao-mincho/TakaoMincho.ttf"    # 日本語フォントのパス
+    fpath = '/usr/share/fonts/truetype/takao-mincho/TakaoMincho.ttf'
 
-    wordcloud = WordCloud(background_color="white", font_path=fpath,
-                          regexp="[\w']+", width=800, height=600).generate(text)
+    wordcloud = WordCloud(background_color='white',
+                          font_path=fpath,
+                          regexp="[\w']+",  # 一文字単語も含む
+                          width=800,
+                          height=600).generate(text)
 
-    # 画像に名前をつける
-    if SONG_NAME == '*':
-        image_name = ARTISTS_NAME + '.png'
+    # 画像ファイル名
+    if title == '*':
+        image_name = artist + '.png'
     else:
-        image_name = SONG_NAME + '.png'
+        image_name = title + '.png'
 
     # 画像を保存
     wordcloud.to_file('./image/'+image_name)   # WordCloudの画像を保存
